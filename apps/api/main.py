4e4c67f95a034e99139f0a645cbfe7db0d405870
MaxymMelnychuk <maxymmelnychuk100@gmail.com>
2026-09-08 17:29:01 +0200

    fix(api): enable frontend api access

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from apps.api.routes.meetings import router
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
+
 
 @app.get("/")
 def root():
